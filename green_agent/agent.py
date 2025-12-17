ef00a6a5c0e15a1eb7481003b4f80e5a23678304
Nick <nicksaban2007@gmail.com>
2025-12-17 14:02:46 -0800

    Implement manual CORS to fix Agent Card loading

--- green_agent/agent.py
+++ green_agent/agent.py
@@ -374,6 +374,13 @@ User message: {user_goal}"""
 
 app = Flask(__name__)
 
+@app.after_request
+def after_request(response):
+    response.headers.add('Access-Control-Allow-Origin', '*')
+    response.headers.add('Access-Control-Allow-Headers', 'Content-Type,Authorization')
+    response.headers.add('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS')
+    return response
+
 green_agent = None
 
 
